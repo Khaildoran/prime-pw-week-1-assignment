221e8f4aeb1870c913168a391b972bcc6631fb47
Khaildoran <sgermscheid@gmail.com>
2020-08-20 18:46:10 -0500

    main obj completed

---
+++ assignment/helloWorld.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+function updateImage(image){
+
+  if (image.src = 'coffeeroaster.jpg' ) {
+      image.src = 'img2.png'
+
+  } else if (image.src = 'img2.png') {
+      image.src = 'coffeeroaster.jpg'
+  }
+
+}
