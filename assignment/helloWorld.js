591ef0982365f3feace28998736f51b151bd09ae
Khaildoran <sgermscheid@gmail.com>
2020-08-21 14:45:19 -0500

    updated image list

--- assignment/helloWorld.js
+++ assignment/helloWorld.js
@@ -6,11 +6,18 @@ console.log('Hello World!')
 function updateImage(){
 
   if (document.getElementById("coffee").alt === 'paradise') {
-      document.getElementById("coffee").src = 'img2.png';
+      document.getElementById("coffee").src = 'smith.png';
       document.getElementById('coffee').alt = 'smith';
 } else if (document.getElementById("coffee").alt === 'smith') {
 
-  document.getElementById("coffee").src = 'coffeeroaster.jpg';
-  document.getElementById('coffee').alt = 'paradise'
+  document.getElementById("coffee").src = 'dogwood.png';
+  document.getElementById('coffee').alt = 'dogwood'
+} else if (document.getElementById("coffee").alt === 'dogwood') {
+
+  document.getElementById("coffee").src = 'spyhouse.png';
+  document.getElementById('coffee').alt = 'spyhouse';
+} else if (document.getElementById('coffee').alt === 'spyhouse') {
+  document.getElementById("coffee").src = 'paradiseroaster.jpg';
+  document.getElementById('coffee').alt = 'paradise';
 }
 }
